a1d9d88bd01bc4ec3c0ccc6eb4c50e88c1194e90
Yonghao Jin <sljinyonghao@hotmail.com>
2018-12-10 21:22:56 -0500

    port number from command line

--- backend/src/Main.hs
+++ backend/src/Main.hs
@@ -15,11 +15,15 @@ import Network.Wai.Handler.Warp
 import qualified Data.Text as T
 import Network.Wai.Middleware.Cors
 
+import System.Environment
+
 import Application
 
 main :: IO ()
 main = runServer "test.sqlite"
 
 runServer :: T.Text -> IO ()
-runServer sqliteFile =
-  run 80 =<< (simpleCors <$> mkApp sqliteFile)
+runServer sqliteFile = do
+  [port] <- getArgs
+  let portN = read port
+  run portN =<< (simpleCors <$> mkApp sqliteFile)
